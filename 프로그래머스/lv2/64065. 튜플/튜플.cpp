fca81deea8f20747d12d82ec567a2c791c8912a0
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2023-10-07 20:37:34 +0900

    [level 2] Title: 튜플, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.cpp
+++ 프로그래머스/lv2/64065. 튜플/튜플.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <cctype>
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -14,31 +13,17 @@ vector<int> solution(string s) {
         if(isdigit(c[i])!=0){
             tmp+=c[i];
         }else if(i>1 && c[i] == '{'){
-            V.resize((++idx)+1);
+            if(V.size()==(idx+1)) V.resize((++idx)+1);
         }else if(tmp!=""){
             V[idx].push_back(stoi(tmp));
             tmp="";
         }
     }
-    int count = 0;
-    for(vector<int> v : V){
-        if(v.size()>0) count++;
-    }
-    sort(V.begin(),V.end(), [](vector<int> v1, vector<int> v2) -> bool{return v1.size()>v2.size();});
-    V.resize(count);
     sort(V.begin(),V.end(), [](vector<int> v1, vector<int> v2) -> bool{return v1.size()<v2.size();});
     vector<int> answer;
     for(vector<int> v : V){
-        for(int i : answer){
-            for(int j = 0; j<v.size(); j++){
-                if(i==v[j]){
-                    v.erase(v.begin()+j);
-                }
-            }
-        }
-        for(int i : v){
-            answer.push_back(i);
-        }
+        for(int i : answer) for(int j = 0; j<v.size(); j++) if(i==v[j]) v.erase(v.begin()+j);
+        for(int i : v) answer.push_back(i);
     }
     return answer;
 }
