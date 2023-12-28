bd5b133f404b7c6447b6480b50f8119c7d9b47ee
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2023-12-29 02:06:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.08 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end(),[](int a, int b){return a>b;});
+    for(int i =0; i<A.size(); i++){
+        answer+= A[i]*B[i];
+    }
+    return answer;
+}
