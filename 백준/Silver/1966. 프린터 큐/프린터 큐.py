529f0ace3626d038f0e7bb50044cf9caffb76b82
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2024-07-12 01:39:48 +0900

    [Silver III] Title: 프린터 큐, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,24 @@
+import sys
+TC = int(sys.stdin.readline().rstrip())
+
+for i in range(TC):
+    N,M = map(int,sys.stdin.readline().split())
+    doc = list(map(int,input().split()))
+    dic = {}
+    q = list(range(N))
+    for i in range(N):
+        dic[i] = doc[i]
+    ret = 0
+    while q:
+        max_v = max(doc)
+        if max_v == dic[q[0]]:
+            ret += 1
+            if q[0] == M:
+                print(ret)
+                q.clear()
+                break
+            else:
+                q.pop(0)
+                doc.remove(max_v)
+        else:
+            q.append(q.pop(0))
