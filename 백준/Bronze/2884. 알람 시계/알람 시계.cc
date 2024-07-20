21dcf3e65b15021865937b2f7ad9707ac1a045e7
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2024-07-20 20:03:46 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int H, M;
+	cin >> H >> M;
+	
+	M -= 45;
+
+	if (M < 0) {
+		H--;
+		M += 60;
+	}
+
+	if (H < 0) {
+		H += 24;
+	}
+	cout << H << " " << M;
+}
